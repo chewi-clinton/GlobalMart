5843b034695a44cff4b68eb545484d86c712c4a4
chewi-clinton <suhchenwi48@gmail.com>
2026-03-08 13:11:17 +0100

    implement routing with NotFound page and update dependencies

--- GlobalMart/src/App.jsx
+++ GlobalMart/src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header.jsx";
 import Hero from "./components/HeroSection.jsx";
 import Gateway from "./components/MarketplaceGateway.jsx";
 import Footer from "./components/Footer.jsx";
-function App() {
+import NotFound from "./pages/NotFound.jsx";
+
+// Home Page Component
+function HomePage() {
   return (
-    <div className="app-container">
-      <Header />
+    <>
       <Hero />
       <Gateway />
       <Footer />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
