ab552a72b670cf64d29bcb43c27eba19f6ae1bb9
chewi-clinton <suhchenwi48@gmail.com>
2026-04-04 12:24:31 +0100

    Add product service

--- backend/services/product_service/config/settings.py
+++ backend/services/product_service/config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
