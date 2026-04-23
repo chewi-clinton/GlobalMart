2c1f0150342e27aa23feb25566b149a2c9447a12
chewi-clinton <suhchenwi48@gmail.com>
2026-04-23 17:08:28 +0100

    k

--- backend/services/product_service/config/wsgi.py
+++ backend/services/product_service/config/wsgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, static_prefix="/media/", root=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "media"))
